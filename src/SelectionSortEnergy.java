07fd14524a28f40b6e9b56546907851543832082
VeriTesEnergyLab <veritesenergylab@gmail.com>
2019-08-27 15:24:16 -0300

    Init commit

--- src/SelectionSortEnergy.java
+++ src/SelectionSortEnergy.java
@@ -1,18 +1,12 @@
+/**
+ *
+ * @author danilo_alvees
+ */
 
 import java.util.Vector;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author danilo
- */
 public class SelectionSortEnergy {
     public static void selectionSortVector(Vector<Integer> numbers) {
         int x = 0,y = 0, z = 0;
